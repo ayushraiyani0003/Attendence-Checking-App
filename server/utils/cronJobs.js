a4b33a5fba820e7c15960b5f6098b94291883165
Ayushraiyani0003 <ayushraiyani03@gmail.com>
2025-03-24 18:09:35 +0530

    cron time change
    change to mid night

--- server/utils/cronJobs.js
+++ server/utils/cronJobs.js
@@ -3,7 +3,7 @@ const { generateDailyAttendance } = require("../controllers/attendanceController
 const { getReportingGroupData } = require("../controllers/reportingGroupController")
 
 // Cron job to generate attendance at 12:00 AM every day
-cron.schedule('00 45 16 * * *', () => { // change 9 to 0 
+cron.schedule('00 00 00 * * *', () => { // change 9 to 0 
   console.log('Generating daily attendance...');
   generateDailyAttendance();
   getReportingGroupData();
