bb801e295bac0f37922d12a3ee000c360c25ac6d
Ayushraiyani0003 <ayushraiyani03@gmail.com>
2025-04-08 06:40:33 +0100

    report function index file

---
+++ server/utils/report/index.js
@@ -0,0 +1,17 @@
+const generateNetHrReport = require('./generateNetHrReport');
+const generateOtHrReport = require('./generateOtHrReport');
+const generateSiteExpenseReport = require('./generateSiteExpenseReport');
+const generateEveningShiftReport = require('./generateEveningShiftReport');
+const generateNightShiftReport = require('./generateNightShiftReport');
+const generateAbsentReport = require('./generateAbsentReport');
+const generateDetailedGroupReport = require('./generateDetailedGroupReport');
+
+module.exports = {
+  generateNetHrReport,
+  generateOtHrReport,
+  generateSiteExpenseReport,
+  generateEveningShiftReport,
+  generateNightShiftReport,
+  generateAbsentReport,
+  generateDetailedGroupReport,
+};
